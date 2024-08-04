d330c87f82d3bb6c2b4c2ae94f1b1a6aa6fd36a5
Amir <soheyl637@gmai.com>
2024-08-04 05:05:19 +0100

    refactor(audio): move playAudioInBackground to translate.js and update imports

--- src/common/translate.js
+++ src/common/translate.js
@@ -1,7 +1,6 @@
 import browser from "webextension-polyfill";
 import log from "loglevel";
 import { getSettings } from "src/settings/settings";
-import { playAudioInBackground } from "../popup/components/ListenButton";
 
 const logDir = "common/translate";
 
@@ -31,6 +30,27 @@ const setHistory = async (
   await browser.storage.session.set({
     [`${sourceLang}-${targetLang}-${translationApi}-${sourceWord}`]: result,
   });
+};
+
+export const playAudioInBackground = async (text, lang) => {
+  const url = `https://translate.google.com/translate_tts?client=tw-ob&q=${encodeURIComponent(
+    text
+  )}&tl=${lang}`;
+
+  try {
+    const response = await fetch(url);
+    const audioData = await response.arrayBuffer();
+
+    const audioContext = new AudioContext();
+    const audioBuffer = await audioContext.decodeAudioData(audioData);
+
+    const sourceNode = audioContext.createBufferSource();
+    sourceNode.buffer = audioBuffer;
+    sourceNode.connect(audioContext.destination);
+    sourceNode.start(0);
+  } catch (error) {
+    console.debug("Error playing audio in BACK:", error);
+  }
 };
 
 const sendRequestToGoogle = async (word, sourceLang, targetLang) => {
--- src/popup/components/ListenButton.js
+++ src/popup/components/ListenButton.js
@@ -21,27 +21,6 @@ const playAudio = async (text, lang) => {
   await audio.play().catch((e) => log.error(logDir, "playAudio()", e, url));
 };
 
-export const playAudioInBackground = async (text, lang) => {
-  const url = `https://translate.google.com/translate_tts?client=tw-ob&q=${encodeURIComponent(
-    text
-  )}&tl=${lang}`;
-
-  try {
-    const response = await fetch(url);
-    const audioData = await response.arrayBuffer();
-
-    const audioContext = new AudioContext();
-    const audioBuffer = await audioContext.decodeAudioData(audioData);
-
-    const sourceNode = audioContext.createBufferSource();
-    sourceNode.buffer = audioBuffer;
-    sourceNode.connect(audioContext.destination);
-    sourceNode.start(0);
-  } catch (error) {
-    console.debug("Error playing audio in BACK:", error);
-  }
-};
-
 export default (props) => {
   const { text, lang } = props;
   const canListen = text && text.length < 200;
@@ -50,7 +29,7 @@ export default (props) => {
   return (
     <button
       className="listenButton"
-      onClick={() => playAudioInBackground(text, lang)}
+      onClick={() => playAudio(text, lang)}
       title={browser.i18n.getMessage("listenLabel")}
     >
       <SpeakerIcon />
