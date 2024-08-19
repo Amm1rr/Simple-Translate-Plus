ce29805bea3d1c811109604baaf2536448f09c35
Amir <amir.khani2810@gmail.com>
2024-08-19 12:09:07 +0100

    refactor: Rename project to Simple Translate Plus and update URLs and metadata

--- src/background/background.js
+++ src/background/background.js
@@ -26,16 +26,16 @@ if (!!browser.contextMenus?.onShown)
   browser.contextMenus.onShown.addListener(onMenusShownListener);
 browser.contextMenus.onClicked.addListener(onMenusClickedListener);
 
-//-- Set-Cookie > SameSite=strict|Lax
-// if (browser.webNavigation) {
-//   browser.webNavigation.onCompleted.addListener(function (details) {
-//     browser.tabs.executeScript({
-//       code: `
-//         document.head.insertAdjacentHTML('beforeend', '<meta http-equiv="Set-Cookie" content="SameSite=strict">');
-//       `,
-//     });
-//   });
-// }
+// Set-Cookie > SameSite=Strict|Lax|None
+if (browser.webNavigation) {
+  browser.webNavigation.onCompleted.addListener(function (details) {
+    browser.tabs.executeScript({
+      code: `
+        document.head.insertAdjacentHTML('beforeend', '<meta http-equiv="Set-Cookie" content="SameSite=None;Secure">');
+      `,
+    });
+  });
+}
 
 const init = async () => {
   await initSettings();
--- src/options/components/SideBar.js
+++ src/options/components/SideBar.js
@@ -4,18 +4,21 @@ import { Link, withRouter } from "react-router-dom";
 import browserInfo from "browser-info";
 import "../styles/SideBar.scss";
 
-const isValidShortcuts = browserInfo().name == "Firefox" && browserInfo().version >= 60;
+const isValidShortcuts =
+  browserInfo().name == "Firefox" && browserInfo().version >= 60;
 
-const SideBar = props => (
+const SideBar = (props) => (
   <div className="sideBar">
     <div className="titleContainer">
       <img src="/icons/64.png" className="logo" />
-      <span className="logoTitle">Simple Translate</span>
+      <span className="logoTitle">Simple Translate+</span>
     </div>
     <ul>
       <li
         className={`sideBarItem ${
-          ["/shortcuts", "/information"].every(path => path != props.location.pathname)
+          ["/shortcuts", "/information"].every(
+            (path) => path != props.location.pathname
+          )
             ? "selected"
             : ""
         }`}
@@ -23,12 +26,24 @@ const SideBar = props => (
         <Link to="/settings">{browser.i18n.getMessage("settingsLabel")}</Link>
       </li>
       {isValidShortcuts && (
-        <li className={`sideBarItem ${props.location.pathname == "/shortcuts" ? "selected" : ""}`}>
-          <Link to="/shortcuts">{browser.i18n.getMessage("shortcutsLabel")}</Link>
+        <li
+          className={`sideBarItem ${
+            props.location.pathname == "/shortcuts" ? "selected" : ""
+          }`}
+        >
+          <Link to="/shortcuts">
+            {browser.i18n.getMessage("shortcutsLabel")}
+          </Link>
         </li>
       )}
-      <li className={`sideBarItem ${props.location.pathname == "/information" ? "selected" : ""}`}>
-        <Link to="/information">{browser.i18n.getMessage("informationLabel")}</Link>
+      <li
+        className={`sideBarItem ${
+          props.location.pathname == "/information" ? "selected" : ""
+        }`}
+      >
+        <Link to="/information">
+          {browser.i18n.getMessage("informationLabel")}
+        </Link>
       </li>
     </ul>
   </div>
